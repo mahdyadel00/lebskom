b8a3a61a093e7d0ddbbfd9acde609e2b0e507652
mahdyadel <mahdyadel00@gmail.com>
2022-07-08 14:10:33 +0200

    categories

--- resources/views/web-views/partials/_single-product.blade.php
+++ resources/views/web-views/partials/_single-product.blade.php
@@ -1,64 +1,84 @@
 @php($overallRating = \App\CPU\ProductManager::get_overall_rating($product->reviews))
-        <div class="product-grid" style="margin-bottom: 12px; box-shadow: 0 1px 3px rgb(0 0 0 / 12%), 0 1px 2px rgb(0 0 0 / 24%);">
-            <div class="product-image" style="max-height: 230px;">
-                <a href="{{route('product',$product->slug)}}" class="image">
-                    <img class="img-1" src="https://ik.imagekit.io/lebskom/thumbnail/{{$product['thumbnail']}}">
-                    {{--  <img class="img-2" src="https://ik.imagekit.io/lebskom/thumbnail//{{$product['thumbnail']}}">  --}}
-                </a>
+{{--  <div class="product-grid"
+    style="margin-bottom: 12px; box-shadow: 0 1px 3px rgb(0 0 0 / 12%), 0 1px 2px rgb(0 0 0 / 24%);">
+    <div class="product-image" style="max-height: 230px;">
+        <a href="{{ route('product', $product->slug) }}" class="image">
+            <img class="img-1" src="https://ik.imagekit.io/lebskom/thumbnail/{{ $product['thumbnail'] }}">
+            <img class="img-2" src="https://ik.imagekit.io/lebskom/thumbnail//{{$product['thumbnail']}}">
+        </a>  --}}
 
-                @if(Request::is('product/*'))
-                <a href="{{route('product',$product->slug)}}" class="product-view"><i class="fa fa-search"></i></a>
+        {{--  @if (Request::is('product/*'))
+            <a href="{{ route('product', $product->slug) }}" class="product-view"><i class="fa fa-search"></i></a>
+        @else
+            <a href="javascript:"onclick="quickView('{{ $product->id }}')" class="product-view"><i
+                    class="fa fa-eye"></i></a>
+        @endif
+    </div>  --}}
+    {{--  <div class="product-content" style="padding: 6px;">
+        <ul class="rating">
+            @for ($inc = 0; $inc < 5; $inc++)
+                @if ($inc < $overallRating[0])
+                    <i class="sr-star czi-star-filled active"></i>
                 @else
-                <a href="javascript:"onclick="quickView('{{$product->id}}')" class="product-view"><i class="fa fa-eye"></i></a>
+                    <i class="sr-star czi-star"></i>
                 @endif
+            @endfor
+            <li class="disable">( {{ $product->reviews_count }} )</li>
+        </ul>
+        <h3 class="title"><a style="font-weight: bold"
+                href="{{ route('product', $product->slug) }}">{{ Str::limit($product['name'], 25) }}</a></h3>
+
+
+        <div style="display: flex; align-items: center;">
+            <div class="price" style="color: black; font-weight: bold">
+                {{ \App\CPU\Helpers::currency_converter(
+                    $product->unit_price - \App\CPU\Helpers::get_product_discount($product, $product->unit_price),
+                ) }}
             </div>
-            <div class="product-content" style="padding: 6px;">
-                <ul class="rating">
-                @for($inc=0;$inc<5;$inc++)
-                    @if($inc<$overallRating[0])
+            @if ($product->discount > 0)
+                <strike style="margin-left: 6px;font-size: 12px!important;color: grey!important;">
+                        {{\App\CPU\Helpers::currency_converter($product->unit_price)}}
+                    </strike>
+                @endif
+        </div>
+
+    </div>  --}}
+{{--  </div>  --}}
+
+
+        <div class="col-md-6 col-lg-4">
+            <div class="product-item">
+                <div class="img-box">
+                    <a href="{{ route('product', $product->slug) }}" class="image">
+                        <img class="img-fluid shop-item-img" src="https://ik.imagekit.io/lebskom/thumbnail//{{$product['thumbnail']}}">
+                    </a>
+
+                </div>
+                @for ($inc = 0; $inc < 5; $inc++)
+                    @if ($inc < $overallRating[0])
                         <i class="sr-star czi-star-filled active"></i>
                     @else
                         <i class="sr-star czi-star"></i>
                     @endif
                 @endfor
-                    <li class="disable">( {{$product->reviews_count}} )</li>
-                </ul>
-                <h3 class="title"><a style="font-weight: bold" href="{{route('product',$product->slug)}}">{{ Str::limit($product['name'], 25) }}</a></h3>
+                <div class="text-box">
+                    <a href="{{ route('product', $product->slug) }}">
+                        <h4>{{ Str::limit($product['name'], 25) }}</h4>
+                    </a>
 
 
-                <div style="display: flex; align-items: center;">
-                <div class="price" style="color: black; font-weight: bold">{{\App\CPU\Helpers::currency_converter(
-                        $product->unit_price-(\App\CPU\Helpers::get_product_discount($product,$product->unit_price))
-                    )}}</div>
-                @if($product->discount > 0)
-                <strike style="margin-left: 6px;font-size: 12px!important;color: grey!important;">
-                        {{\App\CPU\Helpers::currency_converter($product->unit_price)}}
-                    </strike>
-                @endif
+                    <span
+                        class="price">{{ \App\CPU\Helpers::currency_converter(
+                            $product->unit_price - \App\CPU\Helpers::get_product_discount($product, $product->unit_price),
+                        ) }}</span>
+
+                    @if ($product->discount > 0)
+                        <strike style="margin-left: 6px;font-size: 12px!important;color: grey!important;">
+                            {{ \App\CPU\Helpers::currency_converter($product->unit_price) }}
+                        </strike>
+                    @endif
+
                 </div>
-
+                {{--  <span class="padge">New</span>  --}}
             </div>
         </div>
-
-        <div class="col-lg-9">
-            <div class="row">
-                <div class="col-md-6 col-lg-4">
-                    <div class="product-item">
-                        <div class="img-box">
-                            <img class="img-fluid shop-item-img" src="https://ik.imagekit.io/lebskom/thumbnail/{{$product['thumbnail']}}" alt="1 product">
-                        </div>
-                        @for($inc=0;$inc<5;$inc++)
-                        @if($inc<$overallRating[0])
-                            <i class="sr-star czi-star-filled active"></i>
-                        @else
-                            <i class="sr-star czi-star"></i>
-                        @endif
-                    @endfor
-                        <div class="text-box">
-                            <a href="#"><h4>Clothes</h4></a>
-                            <p>New arrival product</p>
-                            <span class="price">AED 250</span>
-                        </div>
-                        <span class="padge">New</span>
-                    </div>
-                </div>
--- resources/views/web-views/products/view.blade.php
+++ resources/views/web-views/products/view.blade.php
@@ -1,36 +1,38 @@
 @extends('layouts.front-end.app')
 
-@section('title',ucfirst($data['data_from']).' products')
+@section('title', ucfirst($data['data_from']) . ' products')
 @section('content')
 
-@include('layouts.front-end.partials._modals')
+    @include('layouts.front-end.partials._modals')
 
-@if(count($featured_products) > 0)
-    <section class="container rtl">
-        <!-- Heading-->
-        <div class="section-header">
-            <div class="feature_header">
-                <span class="for-feature-title">{{ \App\CPU\translate('featured_products')}}</span>
+    @if (count($featured_products) > 0)
+        <section class="container rtl">
+            <!-- Heading-->
+            <div class="section-header">
+                <div class="feature_header">
+                    <span class="for-feature-title">{{ \App\CPU\translate('featured_products') }}</span>
+                </div>
+                <div>
+                    <a class="btn btn-outline-accent btn-sm viw-btn-a"
+                        href="{{ route('products', ['data_from' => 'featured', 'page' => 1]) }}">
+                        {{ \App\CPU\translate('view_all') }}
+                        <i
+                            class="czi-arrow-{{ Session::get('direction') === 'rtl' ? 'left mr-1 ml-n1' : 'right ml-1 mr-n1' }}"></i>
+                    </a>
+                </div>
             </div>
-            <div>
-                <a class="btn btn-outline-accent btn-sm viw-btn-a"
-                   href="{{route('products',['data_from'=>'featured','page'=>1])}}">
-                    {{ \App\CPU\translate('view_all')}}
-                    <i class="czi-arrow-{{Session::get('direction') === "rtl" ? 'left mr-1 ml-n1' : 'right ml-1 mr-n1'}}"></i>
-                </a>
+            <!-- Grid-->
+            <div class="col-lg-9">
+                <div class="row">
+                    @foreach ($featured_products as $product)
+                        @if ($category->id == json_decode($product->category_ids)[0]->id)
+
+
+                            @include('web-views.partials._single-product', ['product' => $product])
+                        @endif
+                    @endforeach
+                </div>
             </div>
-        </div>
-         <!-- Grid-->
-        <div class="row mt-2 mb-3">
-            @foreach($featured_products as $product)
-             @if ($category->id == json_decode($product->category_ids)[0]->id)
-                 <div class="col-xl-2 col-sm-3 col-6" style="margin-bottom: 10px">
-                     @include('web-views.partials._single-product',['product'=>$product])
-                     {{--<hr class="d-sm-none">--}}
-                 </div>
-             @endif
-            @endforeach
-        </div>
-    </section>
-@endif
+        </section>
+    @endif
 @endsection
